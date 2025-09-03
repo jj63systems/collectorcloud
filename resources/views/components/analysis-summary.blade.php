163e2442d2a9007d516a66e8493277b4d592bd65
jj63systems <152405212+jj63systems@users.noreply.github.com>
2025-09-03 16:27:31 +0100

    Removed the issues section - will not use AI for this i don't think

--- resources/views/components/analysis-summary.blade.php
+++ resources/views/components/analysis-summary.blade.php
@@ -39,49 +39,49 @@
     @endif
 
 
-    {{-- Validation Issues Block --}}
-    @if ($rowsAnalysed > 0)
-        <x-filament::section>
-            <x-slot name="heading">
-                Validation Issues
-            </x-slot>
+    {{--    --}}{{-- Validation Issues Block --}}
+    {{--    @if ($rowsAnalysed > 0)--}}
+    {{--        <x-filament::section>--}}
+    {{--            <x-slot name="heading">--}}
+    {{--                Validation Issues--}}
+    {{--            </x-slot>--}}
 
-            <div class="text-sm text-gray-700 mb-2">
-                We processed {{ $rowsAnalysed }} record{{ $rowsAnalysed === 1 ? '' : 's' }} from the spreadsheet
-                @if (empty($validationIssues))
-                    and found no obvious data issues.
-                @else
-                    and found the following potential issues:
-                @endif
-            </div>
+    {{--            <div class="text-sm text-gray-700 mb-2">--}}
+    {{--                We processed {{ $rowsAnalysed }} record{{ $rowsAnalysed === 1 ? '' : 's' }} from the spreadsheet--}}
+    {{--                @if (empty($validationIssues))--}}
+    {{--                    and found no obvious data issues.--}}
+    {{--                @else--}}
+    {{--                    and found the following potential issues:--}}
+    {{--                @endif--}}
+    {{--            </div>--}}
 
-            @if (!empty($validationIssues))
-                <div class="overflow-x-auto">
-                    <table class="min-w-full text-sm text-left">
-                        <tbody>
-                        @foreach ($validationIssues as $pgName => $issue)
-                            <tr class="last:border-none">
-                                <td>
-                                    <div class="p-2">
-                                        {{ $pgName }}
-                                    </div>
-                                </td>
-                                <td class="align-left">
-                                    &nbsp;-&nbsp;
-                                    @if (is_array($issue))
-                                        {!! implode('<br>', array_map('e', $issue)) !!}
-                                    @else
-                                        {{ $issue }}
-                                    @endif
-                                </td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            @endif
-        </x-filament::section>
-    @endif
+    {{--            @if (!empty($validationIssues))--}}
+    {{--                <div class="overflow-x-auto">--}}
+    {{--                    <table class="min-w-full text-sm text-left">--}}
+    {{--                        <tbody>--}}
+    {{--                        @foreach ($validationIssues as $pgName => $issue)--}}
+    {{--                            <tr class="last:border-none">--}}
+    {{--                                <td>--}}
+    {{--                                    <div class="p-2">--}}
+    {{--                                        {{ $pgName }}--}}
+    {{--                                    </div>--}}
+    {{--                                </td>--}}
+    {{--                                <td class="align-left">--}}
+    {{--                                    &nbsp;-&nbsp;--}}
+    {{--                                    @if (is_array($issue))--}}
+    {{--                                        {!! implode('<br>', array_map('e', $issue)) !!}--}}
+    {{--                                    @else--}}
+    {{--                                        {{ $issue }}--}}
+    {{--                                    @endif--}}
+    {{--                                </td>--}}
+    {{--                            </tr>--}}
+    {{--                        @endforeach--}}
+    {{--                        </tbody>--}}
+    {{--                    </table>--}}
+    {{--                </div>--}}
+    {{--            @endif--}}
+    {{--        </x-filament::section>--}}
+    {{--    @endif--}}
 
     {{-- Entities Block --}}
     @if (!empty($entities))
