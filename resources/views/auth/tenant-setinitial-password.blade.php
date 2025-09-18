802fbed24a1b2d37f2f4b928d53d7b5043155af3
jj63systems <152405212+jj63systems@users.noreply.github.com>
2025-09-18 14:23:14 +0100

    minor code cleanup

--- resources/views/auth/tenant-setinitial-password.blade.php
+++ resources/views/auth/tenant-setinitial-password.blade.php
@@ -55,7 +55,7 @@
                     <button type="button"
                             class="absolute right-2 top-1/2 -translate-y-1/2 w-8 h-8 rounded-full bg-gray-100 border border-gray-200
                                    flex items-center justify-center text-gray-500 hover:text-gray-700"
-                            onclick="togglePw('password', this)">
+                            onclick="togglePw('password')">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 24 24"
                              stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -83,7 +83,7 @@
                     <button type="button"
                             class="absolute right-2 top-1/2 -translate-y-1/2 w-8 h-8 rounded-full bg-gray-100 border border-gray-200
                                    flex items-center justify-center text-gray-500 hover:text-gray-700"
-                            onclick="togglePw('password_confirmation', this)">
+                            onclick="togglePw('password_confirmation')">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 24 24"
                              stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
