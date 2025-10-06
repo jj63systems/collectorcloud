cdb49a7f3e44fb9792c677129a2a4bd54ca4ef29
jj63systems <152405212+jj63systems@users.noreply.github.com>
2025-10-06 15:48:00 +0100

    Rename cc_roles & cc_users

--- config/app_permissions.php
+++ config/app_permissions.php
@@ -5,22 +5,20 @@ return [
     'resources' => [
         'cc_locations' => ['view', 'create', 'update', 'delete'],
         'cc_teams' => ['view', 'create', 'update', 'delete'],
-        'cc_users' => ['view', 'create', 'update', 'delete'],
-        'cc_roles' => ['view', 'create', 'update', 'delete'],
+        'users' => ['view', 'create', 'update', 'delete'],
+        'roles' => ['view', 'create', 'update', 'delete'],
     ],
 
     'roles' => [
         'readonly' => [
             'cc_locations.view',
             'cc_teams.view',
-            'cc_users.view',
         ],
 
         'archivist' => [
             'cc_locations.view',
             'cc_locations.create',
             'cc_locations.update',
-            'cc_teams.view',
         ],
 
         'archive_manager' => [
@@ -32,8 +30,8 @@ return [
             'cc_teams.create',
             'cc_teams.update',
             'cc_teams.delete',
-            'cc_users.view',
-            'cc_users.update',
+            'users.view',
+            'users.update',
         ],
 
         'superuser' => [
@@ -45,14 +43,14 @@ return [
             'cc_teams.create',
             'cc_teams.update',
             'cc_teams.delete',
-            'cc_users.view',
-            'cc_users.create',
-            'cc_users.update',
-            'cc_users.delete',
-            'cc_roles.view',
-            'cc_roles.create',
-            'cc_roles.update',
-            'cc_roles.delete',
+            'users.view',
+            'users.create',
+            'users.update',
+            'users.delete',
+            'roles.view',
+            'roles.create',
+            'roles.update',
+            'roles.delete',
         ],
     ],
 
